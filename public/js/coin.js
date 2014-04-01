d06a7458ba14719bfed02429837edecbb3ccc9ce
Ian Hansen <ian@supershabam.com>
2014-04-01 08:01:55 -0400

    such doge

---
+++ public/js/coin.js
@@ -0,0 +1,12 @@
+$(function() {
+  if (window.requestAnimationFrame) {
+    var start = null
+    var coins = []
+    function animate(time) {
+
+    }
+    $(document).on('mousemove', function(e) {
+      // console.log(e)
+    })  
+  }
+})
